0a1f2ccfaeec4419d08d3c450c2bf6e05a5cdb09
Pavlo Bashynskiy <levonet@gmail.com>
2025-08-07 21:30:55 +0300

    Fix lint false positive

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
 ];
